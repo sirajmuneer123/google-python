aa022e069eb7030e3259493c92e0e86e6812f7ad
sirajmuneer123 <sirajmuneer4@gmail.com>
2015-11-04 10:13:59 +0530

    google-python-exercises

---
+++ copyspecial/copyspecial.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+import sys
+import re
+import os
+import shutil
+import commands
+
+"""Copy Special exercise
+"""
+
+# +++your code here+++
+# Write functions and modify main() to call them
+def get_special_paths(directory):
+  name_string = '\n'.join(os.listdir(directory))
+  spec_list = re.findall(r'.*__\w+__.*',name_string)
+  spec_files = []
+  for name in spec_list:
+	if os.path.isfile(name):
+		spec_files.append(os.path.abspath(name))
+  return spec_files
+
+def copy_to(paths,directory):
+  if not os.path.exists(directory):
+	os.makedirs(directory)
+  for path in paths:
+	shutil.copy(path,directory)
+
+def zip_to(paths,zippath):
+  cmd = "zip -j " + zippath 
+  for path in paths:
+	cmd += ' ' + path
+  print "Command to run:", cmd
+  (status, output) = commands.getstatusoutput(cmd)
+  if status:
+	sys.stderr.write(output)
+	sys.exit(1)
+  print output
+
+def main():
+  # This basic command line argument parsing code is provided.
+  # Add code to call your functions below.
+
+  # Make a list of command line arguments, omitting the [0] element
+  # which is the script itself.
+  args = sys.argv[1:]
+  if not args:
+    print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+    sys.exit(1)
+
+  # todir and tozip are either set from command line
+  # or left as the empty string.
+  # The args array is left just containing the dirs.
+  todir = ''
+  if args[0] == '--todir':
+    todir = args[1]
+    del args[0:2]
+
+  tozip = ''
+  if args[0] == '--tozip':
+    tozip = args[1]
+    del args[0:2]
+
+  if len(args) == 0:
+    print "error: must specify one or more dirs"
+    sys.exit(1)
+
+  # +++your code here+++
+  # Call your functions
+  paths = []
+  for arg in args:
+	paths.extend(get_special_paths(arg))
+
+  if todir == '' and tozip == '':
+	print '\n'.join(paths) + '\n'
+
+  if todir:
+	copy_to(paths,todir)
+
+  if tozip:
+	zip_to(paths,tozip)
+
+
+if __name__ == "__main__":
+  main()
